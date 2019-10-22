4755a64e573b28e3d3bfc7243f007282ab48a69d
Daniel di Benedetto <ddb@wwimmo.ch>
2019-10-22 14:54:47 +0200

    reporting und css optimiert

--- src/Reporting.tsx
+++ src/Reporting.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./App.css";
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
+import "./buttons.css";
+import "./hover.css";
+import "./eyes.css";
+import "./mouth.css";
+import "./smiley4.css";
 
 const SURVEYS = gql`
   query AverageRating {
@@ -11,6 +16,26 @@ const SURVEYS = gql`
         avg {
           value
         }
+      }
+    }
+    vierer: ratings_aggregate(where: { value: { _eq: 4 } }) {
+      aggregate {
+        count(columns: value)
+      }
+    }
+    dreier: ratings_aggregate(where: { value: { _eq: 3 } }) {
+      aggregate {
+        count(columns: value)
+      }
+    }
+    zweier: ratings_aggregate(where: { value: { _eq: 2 } }) {
+      aggregate {
+        count(columns: value)
+      }
+    }
+    einser: ratings_aggregate(where: { value: { _eq: 1 } }) {
+      aggregate {
+        count(columns: value)
       }
     }
   }
@@ -24,12 +49,32 @@ export const Reporting: React.FC = () => {
   console.log(data.ratings_aggregate.aggregate);
   return (
     <div className="App">
+      Auswertung der Bewertungen:
       <div className="auswertung">
-        Auswertung der Bewertungen
         <div>Anzahl Bewertungen: {data.ratings_aggregate.aggregate.count} </div>
-        <div>
+        <div className="vierer"></div>
+        <span className="emoji" role="img" aria-label="veryhappyface">
+          😀
+        </span>{" "}
+        {data.vierer.aggregate.count}
+        <div className="dreier"></div>
+        <span className="emoji" role="img" aria-label="veryhappyface">
+          🙂
+        </span>{" "}
+        {data.dreier.aggregate.count}
+        <div className="zweier"></div>
+        <span className="emoji" role="img" aria-label="veryhappyface">
+          🙁
+        </span>{" "}
+        {data.zweier.aggregate.count}
+        <div className="einser"></div>
+        <span className="emoji" role="img" aria-label="veryhappyface">
+          😡
+        </span>{" "}
+        {data.einser.aggregate.count}
+        <div className="Gesamtauswertung">
           Gesamtauswertung:{" "}
-          {data.ratings_aggregate.aggregate.avg.value.toFixed(2)} von 4
+          {data.ratings_aggregate.aggregate.avg.value.toFixed(2)}/4
         </div>
       </div>
     </div>
